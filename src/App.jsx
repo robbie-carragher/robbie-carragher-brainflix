08190003790498a2a8a60b4a787ad50a33fa1379
robbie-carragher <orcafinweb@gmail.com>
2023-11-23 21:10:29 -0800

    Building the gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import videosData from "./data/video-details.json";
 import Header from "./components/Header/Header";
 import VideoComponent from "./components/Video /Video";
 
@@ -9,8 +9,45 @@ function App() {
     <>
       <Header logo='new logo'/>
       <VideoComponent />
+
+      <ul>
+        {videosData.map((videoData) => {
+          console.log(videoData);
+          return <li key={videoData.id}>{videoData.title}</li>;
+        })}
+      </ul>
+
     </>
   );
 }
 
 export default App;
+
+
+
+
+
+// import videosData from "./data/video-details.json";
+// import Header from "./components/Header/Header";
+// // import VideoComponent from "./components/Video /Video";
+
+// import "./App.scss";
+
+// function App() {
+//   console.log(videosData);
+//   return (
+//     <>
+//       <Header logo="new logo" />
+//       {/* <VideoComponent /> */}
+
+//       <ul>
+//         {videosData.map((videoData) => {
+//           console.log(videoData);
+//           return <li key={videoData.id}>{videoData.title}</li>;
+//         })}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default App;
