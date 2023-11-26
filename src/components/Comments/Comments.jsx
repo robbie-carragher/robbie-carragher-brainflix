5ba436427025d3047d9de7e62d5b10c72b41d1e7
robbie-carragher <orcafinweb@gmail.com>
2023-11-25 17:58:31 -0800

    Styling the mobile

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,11 +1,20 @@
+
 import "./Comments.scss";
 
 export default function Comments({ videoData }) {
   const { title, comments } = videoData;
 
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+  };
+
   return (
     <section className="comments-section">
-      <h3>3 Comments</h3>
+      <h3>{comments.length} Comments</h3>
 
       <section>
         <form id="commentForm" className="comments">
@@ -21,7 +30,7 @@ export default function Comments({ videoData }) {
 
             <div className="comments__input-wrap">
               <div className="comments__label comments__label--move">
-                <label htmlFor="commentArea">COMMENT</label>
+                <label htmlFor="commentArea">JOIN THE CONVERSATION</label>
               </div>
               <div className="comments__text">
                 <textarea
@@ -33,45 +42,37 @@ export default function Comments({ videoData }) {
               </div>
               <div className="comments-submit-wrap">
                 <button type="submit" className="comments__submit-btn">
-                  <p className="comments__innerText">JOIN THE CONVERSATION</p>
-                  {/* COMMENT */}
+                  <p className="comments__innerText">COMMENT</p>
                 </button>
               </div>
             </div>
           </div>
 
-          <ul id="error-messages">
-            {/* Error messages will be inserted here */}
-          </ul>
+          <ul id="error-messages"></ul>
         </form>
 
-        <ul className="comment-area">{/* Comments will be inserted here */}</ul>
+        <ul className="comment-area"></ul>
       </section>
-
-      {/* Rest of your code */}
 
       {comments.length > 0 ? (
         <div className="comments-area">
           <ul className="comments-area__list">
-            {comments.map((comment) => {
-              return (
-                <li key={comment.id} className="comments-area__item">
-                  <div className="comments-area__avatar-preview"></div>
+            {comments.map((comment) => (
+              <li key={comment.id} className="comments-area__item">
+                <div className="comments-area__avatar-preview"></div>
 
-                  <div className="comments-area__comments-wrap">
-                    <div className="comments-area__data-wrap">
-                      <h3 className="comment-area__title"> {comment.name}</h3>
-                      <p className="comment-area__data">{comment.timestamp}</p>
-                    </div>
-
-                    <div className="comment-area__comments">
-                      {" "}
-                      {comment.comment}
-                    </div>
+                <div className="comments-area__comments-wrap">
+                  <div className="comments-area__data-wrap">
+                    <h3 className="comment-area__title"> {comment.name}</h3>
+                    <p className="comment-area__data">{formatDate(comment.timestamp)}</p>
                   </div>
-                </li>
-              );
-            })}
+
+                  <div className="comment-area__comments">
+                    {comment.comment}
+                  </div>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       ) : (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,45 @@
-
-
 import "./Header.scss";
-import "./../../styles/partials/_global.scss"
+import "./../../styles/partials/_global.scss";
 import avatarImage from "./../../../src/assets/images/Mohan-muruge.jpg";
 import logoImage from "./../../../src/assets/images/Logo/BrainFlix-logo.svg";
 
 import FileUploadComponent from "../upload button";
 
 const Header = () => {
-    return (
-
-        <header className="header">
-        <nav className=" header__links--left">
-          <a href="/"> <img src={logoImage} alt="Logo" className="header__links"></img></a>         
-        </nav>
-        <div className="header__links header__links--right">
-    
-        {/* <input type="search" id="search-box" placeholder="Search..."/> */}
+  return (
+    <header className="header">
+      <nav className=" header__links--left">
+        <a href="/"> <img src={logoImage} alt="Logo" className="header__links"></img></a>
+      </nav>
+      <div className="header__links header__links--right">
         <div className="search-container">
-    <input type="search" id="search-box" placeholder="Search..."/>
-</div>
-
-< FileUploadComponent/>
- 
-          
- 
-          <img src={avatarImage} alt="Avatar Image" className="header__avatar-preview"></img>
-          
-         
+          <input type="search" id="search-box" placeholder="Search..." />
+          <img src={avatarImage}  alt="Avatar Image" className="header__avatar-preview"></img>
         </div>
+      </div>
+     <div> <FileUploadComponent /></div>
+    </header>
 
+
+
+
+
+    // <header className="header">
+    //   <nav className=" header__links--left">
+    //     <a href="/"> <img src={logoImage} alt="Logo" className="header__links"></img></a>
+    //   </nav>
+    //   <div className="header__links header__links--right">
+    //     <div className="search-container">
+    //       <input type="search" id="search-box" placeholder="Search..." />
+    //     </div>
+
+    //     <FileUploadComponent />
+
+    //     <img src={avatarImage}  alt="Avatar Image" className="header__avatar-preview"></img>
         
-      </header>
-
-
-    )
-}
+    //   </div>
+    // </header>
+  );
+};
 
 export default Header;
