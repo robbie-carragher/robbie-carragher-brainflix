c0ada3abac896d6d7b26e7a606677858b3131db5
robbie-carragher <orcafinweb@gmail.com>
2023-12-10 18:45:44 -0800

    Set likes added the the Video Component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./Video.scss";
 
 export default function VideoComponent({ videosData, selectedVideoData }) {
-  // Select the first video as default if no video is currently selected
   const videoToShow = selectedVideoData || (videosData && videosData[0]);
+
+  const [likes, setLikes] = useState(videoToShow ? videoToShow.likes : "0");
+
+  const handleLike = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:8080/videos/${videoToShow.id}/likes`
+      );
+      if (response.status === 200) {
+        setLikes(response.data.likes);
+      } else {
+        console.error("Failed to update like");
+      }
+    } catch (error) {
+      console.error("Error updating like:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (videoToShow) {
+      setLikes(videoToShow.likes);
+    }
+  }, [videoToShow]);
 
   if (!videoToShow) {
     return <div>Video is Loading...</div>;
@@ -10,13 +34,19 @@ export default function VideoComponent({ videosData, selectedVideoData }) {
 
   return (
     <section className="video-section">
-<div className="video-section_wrap">
-<video
-        className="video-section__fullwidth"
-        controls
-        poster={videoToShow.image}
-      ></video>
-</div>
+      <div className="video-section_wrap">
+        <video
+          className="video-section__fullwidth"
+          controls
+          poster={videoToShow.image}
+        ></video>
+        <div>
+          <button onClick={handleLike} className="video-section__likes-btn">
+            Like
+          </button>
+          <span className="video-section__likes">Likes: {likes}</span>
+        </div>
+      </div>
     </section>
   );
 }
